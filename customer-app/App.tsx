d10e566b1daa91564fb6917e94e03eaa1a63d461
Hamza <hamzabenmh@gmail.com>
2025-11-15 19:06:30 +0100

    fix 2

--- customer-app/App.tsx
+++ customer-app/App.tsx
@@ -1,38 +1,23 @@
 import React from 'react';
-import { StyleSheet, View, ViewStyle } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ConnectionStatus } from './components/ConnectionStatus';
 import { HomeScreen } from './screens/HomeScreen';
-import { OrdersScreen } from './screens/OrdersScreen';
-import { ProfileScreen } from './screens/ProfileScreen';
-
-const Tab = createBottomTabNavigator();
-
-interface Styles {
-  container: ViewStyle;
-}
+import { colors } from './theme/colors';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <ConnectionStatus />
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Orders" component={OrdersScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ConnectionStatus />
+      <HomeScreen />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-const styles = StyleSheet.create<Styles>({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.background,
   },
 });
